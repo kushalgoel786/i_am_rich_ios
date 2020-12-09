201805ddc6b9d5aeb91c90c2086d0ddc85f4c5d7
Kushal Goel <64199835+kushalgoel786@users.noreply.github.com>
2020-12-09 14:55:00 +0530

    Add files via upload

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kapil Kedar on 31/03/20.
+//  Copyright © 2020 Kushal Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
